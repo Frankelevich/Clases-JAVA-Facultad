261ce7a5ed1156a09e40dfc3e9ff04654ab0822a
Frankelevich <franco.limberti0308@gmail.com>
2022-08-28 02:43:10 -0300

    Ejercicio 5

---
+++ Ejercicio 5/src/Ejercicios_LAB2/Main.java
@@ -0,0 +1,9 @@
+package Ejercicios_LAB2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x= ((6.0/5.0)-(2.0/3.0)*(7.0/2.0)+(2.0/30.0))/((1.0/3.0)/5.0);
+        System.out.println(x);
+    }
+}
