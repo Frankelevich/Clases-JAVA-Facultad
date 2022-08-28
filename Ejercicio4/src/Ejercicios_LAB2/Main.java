b86e09ec6f74ef4cb613f83f8b3f701ead2934b7
Frankelevich <franco.limberti0308@gmail.com>
2022-08-27 23:13:46 -0300

    TUP

---
+++ Ejercicio4/src/Ejercicios_LAB2/Main.java
@@ -0,0 +1,11 @@
+package Ejercicios_LAB2;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String nombre="Franco";
+        int edad=22;
+        double salario= 11999.99;
+        boolean carnet=true;
+    }
+}
